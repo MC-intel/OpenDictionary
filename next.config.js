722427594e37f819038e4a856f9f690fe221ac6f
Michael C. <68439689+MC-intel@users.noreply.github.com>
2024-08-22 00:07:28 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
   basePath: '/OpenDictionary', 
   assetPrefix: '/OpenDictionary', 
+  async rewrites() {
+    return [
+      {
+        source: '/data/:path*',
+        destination: '/public/data/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
